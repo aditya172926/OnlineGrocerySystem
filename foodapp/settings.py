0b53733d7fcc37dbd749f4c120bdb6b07627ae47
Aditya <aditya26sg@gmail.com>
2020-10-16 18:05:50 +0530

    Some changes in settings

--- foodapp/settings.py
+++ foodapp/settings.py
@@ -81,7 +81,16 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Shivbaba26$',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
